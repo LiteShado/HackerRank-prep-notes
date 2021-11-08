4bb400858338b75ab578c1dcdc9b4ca99c1d15ae
Natalie <theliteshado@icloud.com>
2021-11-07 20:26:20 -0500

    basic java run through

---
+++ Java-Excercises.java
@@ -0,0 +1,27 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+/////
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
