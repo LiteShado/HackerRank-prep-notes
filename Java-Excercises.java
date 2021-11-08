2e0a2fc3cea842e960e4be830023acb3efb31f9b
Natalie <theliteshado@icloud.com>
2021-11-07 21:22:28 -0500

    exponent print out

--- Java-Excercises.java
+++ Java-Excercises.java
@@ -119,3 +119,31 @@ public class Solution {
         }
     }
 }
+
+// We use the integers , , and  to create the following series:
+
+// You are given  queries in the form of , , and . For each query, print the series corresponding to the given , , and  values as a single line of  space-separated integers.
+
+// Input Format
+
+// The first line contains an integer, , denoting the number of queries.
+// Each line  of the  subsequent lines contains three space-separated integers describing the respective , , and  values for that query.
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int c = a;
+            for(int j=0;j<n;j++){
+                c += Math.pow(2, j)*b;
+                System.out.printf("%s ",c);
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
